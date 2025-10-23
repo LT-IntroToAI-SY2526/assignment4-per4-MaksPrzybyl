2ab276da2deb9d960eb8489e028b30bb07ade0bc
Maks Przybyl <mfprzybyl@cps.edu>
2025-10-23 11:31:21 -0500

    dog

--- a4.py
+++ a4.py
@@ -88,5 +88,5 @@ if __name__ == "__main__":
 
     print("All tests passed!")
 
-    # uncomment to play!
-    # play_tic_tac_toe()
+
+    play_tic_tac_toe()
--- notes.py
+++ notes.py
@@ -2,3 +2,21 @@
 
 # (define-struct dog [fur_color name age favorite_food])
 
+class Dog:
+    "Simple class to learn concepts"
+    
+       
+    def __init__(self, breed, fur_color, name, age):
+        self.breed = breed
+        self.fur_color = fur_color
+        self.name = name
+        self.age = age
+
+
+maks_dog = Dog("border_collie","black_white","Juno","9")
+
+
+
+def __str__ (self):
+    return f"{self.name} is a {self.age} year old {self.fur_color} {self.breed}" 
+print(maks_dog)
